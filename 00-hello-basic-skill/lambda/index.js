749533aa645b512738c1e2fe1a540637b91872b2
bklug <bklug@bcit.ca>
2018-10-05 19:00:06 -0700

    Add files via upload
    Added new, required, built-in intents.

--- 00-hello-basic-skill/lambda/index.js
+++ 00-hello-basic-skill/lambda/index.js
@@ -9,11 +9,24 @@ exports.handler = function(event, context, callback){
 var handlers = {
 
   'LaunchRequest': function () {
-    this.emit(':ask', 'Welcome to B C NET 2018', 'Try saying hello');
+    this.emit(':ask', 'Welcome to the blood test results skill. Try saying hi or hello', 'Try saying hi or hello');
   },
-
   'Hello': function () {
-    this.emit(':tell', 'Hello there!');
+    this.emit(':tell', 'Hello there! Thanks for using this skill. Good bye!');
+  },
+  'AMAZON.FallbackIntent' : function () {
+    this.emitWithState('AMAZON.HelpIntent');
+  },
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', 'cancel intent');
+  }, 
+  'AMAZON.HelpIntent' : function () {
+    this.emit(':ask', 'Help intent. Try saying hi or hello',  'Try saying hi or hello');
+  },
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', 'stop intent');
+  },
+  'AMAZON.NavigateHomeIntent': function () {
+    this.emit(':tell', 'navigate home intent');
   }
-
 };
