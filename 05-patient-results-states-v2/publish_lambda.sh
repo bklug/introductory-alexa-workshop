f2caf4b1ee7ddb21da056a24589be99d2c07618e
bklug <bklug@bcit.ca>
2019-11-13 11:27:19 -0800

    Update

--- 05-patient-results-states-v2/publish_lambda.sh
+++ 05-patient-results-states-v2/publish_lambda.sh
@@ -3,4 +3,4 @@ cd lambda
 npm install
 zip -r ../lambda.zip *
 cd ..
-aws lambda update-function-code --function-name bcnet-2018 --zip-file fileb://lambda.zip 
+aws lambda update-function-code --function-name alexa-blood-test-results --zip-file fileb://lambda.zip 
